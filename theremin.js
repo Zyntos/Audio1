68c8523eb06481a0f4f4f535a51ed554fdc3d328
Darren Landig <zyrielsleepwalker@gmail.com>
2017-10-09 18:20:45 +0200

    Theremin

---
+++ theremin.js
@@ -0,0 +1,42 @@
+
+var context = new AudioContext();
+var oscillatorNode = context.createOscillator();
+var gainNode = context.createGain();
+
+oscillatorNode.connect(gainNode);
+gainNode.connect(context.destination);
+
+
+gainNode.gain.value = 0.03;
+oscillatorNode.frequency.value = 880;
+
+
+
+
+var mousedown = false;
+
+document.body.addEventListener('mousedown', function(e){
+    mousedown= true;
+    oscillatorNode = context.createOscillator();
+    oscillatorNode.connect(gainNode);
+    oscillatorNode.start(context.currentTime);
+
+
+});
+
+document.body.addEventListener('mousemove', function(e){
+    if (mousedown){
+        console.log(e.clientX);
+        console.log(e.clientY);
+        oscillatorNode.frequency.value = (e.clientX*3);
+        gainNode.gain.value = (e.clientY)/2000;
+
+        console.log(window.innerHeight);
+        console.log(window.innerWidth);
+    }
+});
+
+document.body.addEventListener('mouseup', function(e){
+    mousedown= false;
+    oscillatorNode.stop(context.currentTime);
+});
